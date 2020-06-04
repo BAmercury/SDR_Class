14197990f62fe3188cb884c9c513fefd53c11f3b
Brian Amin <ba555@drexel.edu>
2020-06-04 07:12:36 -0400

    added QPSK scheme with jammer no relay

--- FinalProject/sdr_T580_final_project.py
+++ FinalProject/sdr_T580_final_project.py
@@ -78,7 +78,7 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
         self.noise_volt = noise_volt = 0.5
         self.freq_offset = freq_offset = 0
         self.eq_gain = eq_gain = 0.01
-        self.delay = delay = 116
+        self.delay = delay = 68
         self.arity = arity = 4
 
         ##################################################
@@ -142,7 +142,7 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
             self.controls_grid_layout_1.setRowStretch(r, 1)
         for c in range(1, 2):
             self.controls_grid_layout_1.setColumnStretch(c, 1)
-        self._delay_range = Range(0, 200, 1, 116, 200)
+        self._delay_range = Range(0, 200, 1, 68, 200)
         self._delay_win = RangeWidget(self._delay_range, self.set_delay, 'Delay', "counter_slider", float)
         self.top_grid_layout.addWidget(self._delay_win, 1, 0, 1, 1)
         for r in range(1, 2):
@@ -326,6 +326,47 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.qtgui_const_sink_x_0_0_0 = qtgui.const_sink_c(
+        	1024, #size
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_const_sink_x_0_0_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0_0_0.set_y_axis(-2, 2)
+        self.qtgui_const_sink_x_0_0_0.set_x_axis(-2, 2)
+        self.qtgui_const_sink_x_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0_0_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0_0_0.enable_grid(False)
+        self.qtgui_const_sink_x_0_0_0.enable_axis_labels(True)
+
+        if not True:
+          self.qtgui_const_sink_x_0_0_0.disable_legend()
+
+        labels = ['TX_Jammed', 'TX', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "blue", "red", "red", "red",
+                  "red", "red", "red", "red", "red"]
+        styles = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0_0_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0_0_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0_0_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0_0_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0_0_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_0_0_win)
         self.qtgui_const_sink_x_0_0 = qtgui.const_sink_c(
         	1024, #size
         	"", #name
@@ -408,23 +449,10 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.digital_pfb_clock_sync_xxx_0_0 = digital.pfb_clock_sync_ccf(samp_per_symbol, timing_loop_bw, (rrc_taps), nfilts, nfilts/2, 1.5, 2)
         self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(samp_per_symbol, timing_loop_bw, (rrc_taps), nfilts, nfilts/2, 1.5, 2)
-        self.digital_map_bb_0_0 = digital.map_bb(([0,1,2,3]))
         self.digital_map_bb_0 = digital.map_bb(([0,1,2,3]))
-        self.digital_diff_decoder_bb_0_0 = digital.diff_decoder_bb(4)
         self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(4)
-        self.digital_costas_loop_cc_0_0 = digital.costas_loop_cc(phase_bw, arity, False)
         self.digital_costas_loop_cc_0 = digital.costas_loop_cc(phase_bw, arity, False)
-        self.digital_constellation_modulator_0_0 = digital.generic_mod(
-          constellation=qpsk,
-          differential=True,
-          samples_per_symbol=samp_per_symbol,
-          pre_diff_code=True,
-          excess_bw=rolloff,
-          verbose=False,
-          log=False,
-          )
         self.digital_constellation_modulator_0 = digital.generic_mod(
           constellation=qpsk,
           differential=True,
@@ -434,9 +462,7 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
           verbose=False,
           log=False,
           )
-        self.digital_constellation_decoder_cb_0_0 = digital.constellation_decoder_cb(qpsk)
         self.digital_constellation_decoder_cb_0 = digital.constellation_decoder_cb(qpsk)
-        self.digital_cma_equalizer_cc_0_0 = digital.cma_equalizer_cc(15, 1, eq_gain, 2)
         self.digital_cma_equalizer_cc_0 = digital.cma_equalizer_cc(15, 1, eq_gain, 2)
         self.channels_channel_model_0 = channels.channel_model(
         	noise_voltage=noise_volt,
@@ -446,39 +472,37 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
         	noise_seed=0,
         	block_tags=False
         )
-        self.blocks_unpack_k_bits_bb_0_1 = blocks.unpack_k_bits_bb(2)
         self.blocks_unpack_k_bits_bb_0_0 = blocks.unpack_k_bits_bb(8)
         self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(2)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_pack_k_bits_bb_0_0 = blocks.pack_k_bits_bb(8)
         self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, 'C:\\Users\\brian\\Documents\\SDR_Class\\FinalProject\\data_source_matlab.bin', True)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_sink_1_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\brian\\Documents\\SDR_Class\\FinalProject\\data\\Trial8_RX_byte.bin', False)
+        self.blocks_file_sink_1_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\brian\\Documents\\SDR_Class\\FinalProject\\data\\Trial7_RX_byte.bin', False)
         self.blocks_file_sink_1_0.set_unbuffered(False)
-        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\brian\\Documents\\SDR_Class\\FinalProject\\data\\Trial8_TX_byte.bin', False)
+        self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\brian\\Documents\\SDR_Class\\FinalProject\\data\\Trial7_TX_byte.bin', False)
         self.blocks_file_sink_1.set_unbuffered(False)
         self.blocks_delay_0 = blocks.delay(gr.sizeof_float*1, int(delay))
         self.blocks_char_to_float_0_0 = blocks.char_to_float(1, 1)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
-        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, 2000, 1, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, 2000, 1, 0)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_char_to_float_0, 0), (self.qtgui_time_sink_x_0_0, 0))
         self.connect((self.blocks_char_to_float_0_0, 0), (self.blocks_delay_0, 0))
         self.connect((self.blocks_delay_0, 0), (self.qtgui_time_sink_x_0_0, 1))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.digital_pfb_clock_sync_xxx_0_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))
+        self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_const_sink_x_0_0_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_freq_sink_x_1, 0))
         self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_unpack_k_bits_bb_0_0, 0))
         self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.digital_constellation_modulator_0, 0))
-        self.connect((self.blocks_pack_k_bits_bb_0_0, 0), (self.digital_constellation_modulator_0_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.channels_channel_model_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_const_sink_x_0_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
@@ -486,24 +510,16 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.blocks_file_sink_1_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0_0, 0), (self.blocks_char_to_float_0_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0_0, 0), (self.blocks_file_sink_1, 0))
-        self.connect((self.blocks_unpack_k_bits_bb_0_1, 0), (self.blocks_pack_k_bits_bb_0_0, 0))
-        self.connect((self.channels_channel_model_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.blocks_multiply_xx_0, 1))
         self.connect((self.digital_cma_equalizer_cc_0, 0), (self.digital_costas_loop_cc_0, 0))
-        self.connect((self.digital_cma_equalizer_cc_0_0, 0), (self.digital_costas_loop_cc_0_0, 0))
         self.connect((self.digital_constellation_decoder_cb_0, 0), (self.digital_map_bb_0, 0))
-        self.connect((self.digital_constellation_decoder_cb_0_0, 0), (self.digital_map_bb_0_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.digital_constellation_modulator_0_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.digital_constellation_decoder_cb_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_const_sink_x_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_freq_sink_x_2, 0))
-        self.connect((self.digital_costas_loop_cc_0_0, 0), (self.digital_constellation_decoder_cb_0_0, 0))
         self.connect((self.digital_diff_decoder_bb_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
-        self.connect((self.digital_diff_decoder_bb_0_0, 0), (self.blocks_unpack_k_bits_bb_0_1, 0))
         self.connect((self.digital_map_bb_0, 0), (self.digital_diff_decoder_bb_0, 0))
-        self.connect((self.digital_map_bb_0_0, 0), (self.digital_diff_decoder_bb_0_0, 0))
         self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.digital_cma_equalizer_cc_0, 0))
-        self.connect((self.digital_pfb_clock_sync_xxx_0_0, 0), (self.digital_cma_equalizer_cc_0_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "sdr_T580_final_project")
@@ -529,7 +545,6 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
 
     def set_timing_loop_bw(self, timing_loop_bw):
         self.timing_loop_bw = timing_loop_bw
-        self.digital_pfb_clock_sync_xxx_0_0.set_loop_bandwidth(self.timing_loop_bw)
         self.digital_pfb_clock_sync_xxx_0.set_loop_bandwidth(self.timing_loop_bw)
 
     def get_time_offset(self):
@@ -556,14 +571,13 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_1.set_frequency_range(0, self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
 
     def get_rrc_taps(self):
         return self.rrc_taps
 
     def set_rrc_taps(self, rrc_taps):
         self.rrc_taps = rrc_taps
-        self.digital_pfb_clock_sync_xxx_0_0.update_taps((self.rrc_taps))
         self.digital_pfb_clock_sync_xxx_0.update_taps((self.rrc_taps))
 
     def get_rolloff(self):
@@ -583,7 +597,6 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
 
     def set_phase_bw(self, phase_bw):
         self.phase_bw = phase_bw
-        self.digital_costas_loop_cc_0_0.set_loop_bandwidth(self.phase_bw)
         self.digital_costas_loop_cc_0.set_loop_bandwidth(self.phase_bw)
 
     def get_noise_volt(self):
@@ -605,7 +618,6 @@ class sdr_T580_final_project(gr.top_block, Qt.QWidget):
 
     def set_eq_gain(self, eq_gain):
         self.eq_gain = eq_gain
-        self.digital_cma_equalizer_cc_0_0.set_gain(self.eq_gain)
         self.digital_cma_equalizer_cc_0.set_gain(self.eq_gain)
 
     def get_delay(self):
